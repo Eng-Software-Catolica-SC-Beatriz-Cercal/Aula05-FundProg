4d7544484d1134fa42f8c0bdbdbbe1db8a5e3abe
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-01 11:04:52 -0300

    Exercício 1 finalizado

--- exercicio1.c
+++ exercicio1.c
@@ -4,11 +4,18 @@
 int main(void){
     //Variáveis
     int numero;
+    float inteiro;
     //Quais são as entradas de dados?
     printf("Digite um número:");
-    scanf("%d", &numero);
+    scanf("%f", &inteiro);
     fflush(stdin);
     //Processamento e saída de dados:
-    
+    numero = (int)inteiro;
+
+    if (inteiro == numero){
+        printf("O numero e inteiro");
+    }else{
+        printf("O numero e quebrado");
+    }
     return 0;
 }
