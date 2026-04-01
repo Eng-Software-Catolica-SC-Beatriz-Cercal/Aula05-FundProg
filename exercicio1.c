59214821b4b0d8e446f249d126dc79224f124936
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-01 10:56:00 -0300

    Iniciando exercício 1

---
+++ exercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void){
+    //Variáveis
+    int numero;
+    //Quais são as entradas de dados?
+    printf("Digite um número:");
+    scanf("%d", &numero);
+    fflush(stdin);
+    //Processamento e saída de dados:
+
+    return 0;
+}
